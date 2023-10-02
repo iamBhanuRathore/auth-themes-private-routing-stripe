e6d2d8f305f14ac07d9f0a8d946b6cf8923a6430
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2023-10-02 23:06:19 +0530

    ..

--- components/Switch/ThemeSwitcher.tsx
+++ components/Switch/ThemeSwitcher.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { useTheme } from "next-themes"
-import { Button } from '@nextui-org/react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@nextui-org/react";
 const ThemeSwitcher = () => {
-    const [mounted, setMounted] = useState(false)
-    const { theme, setTheme, resolvedTheme, systemTheme } = useTheme();
-    // console.log({ theme, resolvedTheme, systemTheme })
-    useEffect(() => {
-        setMounted(true);
-        setTheme(resolvedTheme || "light");
-    }, []);
-    if (!mounted) return null
-    return (
-        <div className='flex space-x-3'>
-            <Button onClick={() => setTheme("dark")}>Dark</Button>
-            <Button onClick={() => setTheme("light")}>Light</Button>
-            <Button onClick={() => setTheme("pink")}>Pink</Button>
-        </div>
-    )
-}
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme, systemTheme } = useTheme();
+  // console.log({ theme, resolvedTheme, systemTheme })
+  useEffect(() => {
+    setMounted(true);
+    setTheme(resolvedTheme || "light");
+  }, [setTheme, setMounted, resolvedTheme]);
+  if (!mounted) return null;
+  return (
+    <div className="flex space-x-3">
+      <Button onClick={() => setTheme("dark")}>Dark</Button>
+      <Button onClick={() => setTheme("light")}>Light</Button>
+      <Button onClick={() => setTheme("pink")}>Pink</Button>
+    </div>
+  );
+};
 
-export default ThemeSwitcher
+export default ThemeSwitcher;
